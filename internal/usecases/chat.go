cfe32163cb9e8f7d1320ccba111a4d228c32043b
DENFNC <andreev.gamepro@gmail.com>
2025-10-25 20:16:24 +0300

    chore: stub chat handler/usecase for lint

--- internal/usecases/chat.go
+++ internal/usecases/chat.go
@@ -1,10 +1,34 @@
+// Package usecases contains application business logic.
 package usecases
 
+import (
+	"context"
+	"errors"
+)
+
+// Eventbus defines a transport for delivering chat messages.
+type Eventbus interface {
+	WriteMessage(ctx context.Context, msg []byte) error
+}
+
+// ChatUsecase dispatches chat events to the underlying event bus.
 type ChatUsecase struct {
+	eventbus Eventbus
 }
 
-func NewChatUsecase() *ChatUsecase {
-	return &ChatUsecase{}
+// NewChatUsecase wires the use case with provided event bus.
+func NewChatUsecase(bus Eventbus) *ChatUsecase {
+	return &ChatUsecase{eventbus: bus}
 }
 
-func (uc *ChatUsecase) SendMessage(userID string, message string) error { return nil }
+// SendMessage delivers chat messages to the event bus.
+func (uc *ChatUsecase) SendMessage(userID string, message string) error {
+	_ = userID
+	_ = message
+
+	if uc.eventbus == nil {
+		return errors.New("chat usecase: eventbus is nil")
+	}
+
+	return errors.New("chat usecase send message not implemented")
+}
